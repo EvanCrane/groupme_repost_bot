98c1438ff53a5d6a9a04f9186af226d24e0d93fc
Evan Crane <evanf.crane@gmail.com>
2018-10-20 03:54:51 -0400

    Refactoring filepath defs

---
+++ bot/filepaths.py
@@ -0,0 +1,18 @@
+# filepaths.py
+import os.path
+
+
+def define_filepaths():
+    filepaths = {}
+    root_path = os.path.dirname("AppData/")
+    filepaths["http_params"] = root_path + "/http_params.json"
+    filepaths["member_data"] = root_path + "/member_data.json"
+    filepaths["response"] = root_path + "/response.json"
+    return filepaths
+
+
+def verify_file(file_path):
+    if os.path.exists(file_path) or os.path.getsize(file_path) > 0:
+        return True
+    else:
+        raise ValueError("AppData file missing or it is empty!")
--- bot/read_appdata.py
+++ bot/read_appdata.py
@@ -1,6 +1,7 @@
 # read_appdata.py
 import json
-import os.path
+from filepaths import define_filepaths
+from filepaths import verify_file
 from models import RequestParams
 
 
@@ -8,19 +9,10 @@ def read_appdata():
     appdata = {}
     filepaths = define_filepaths()
     appdata["http_params"] = get_http_params(filepaths.get("http_params"))
-    #appdata["user_data"] = get_user_data(filepaths.get("user_data"))
+    #appdata["member_data"] = get_member_data(filepaths.get("member_data"))
     #appdata["response"] = get_response(filepaths.get("response"))
     objects = bind_data_to_objects(appdata)
     return objects
-
-
-def define_filepaths():
-    filepaths = {}
-    root_path = os.path.dirname("AppData/")
-    filepaths["http_params"] = root_path + "/http_params.json"
-    filepaths["user_data"] = root_path + "/user_data.json"
-    filepaths["response"] = root_path + "/response.json"
-    return filepaths
 
 
 def get_http_params(path):
@@ -30,7 +22,7 @@ def get_http_params(path):
     return json_data
 
 
-def get_user_data(path):
+def get_member_data(path):
     with open(path) as json_file:
         json_data = json.load(json_file)
     return json_data
@@ -42,15 +34,10 @@ def get_response(path):
     return json_data
 
 
-def verify_file(file_path):
-    if os.path.exists(file_path) or os.path.getsize(file_path) > 0:
-        return True
-    else:
-        raise ValueError("AppData file missing or it is empty!")
-
 def bind_data_to_objects(appdata):
     objects = {}
     request_params = (appdata.get("http_params"))["request_params"]
-    request_params_obj = RequestParams(request_params.get("token"), request_params.get("group_id"))
+    request_params_obj = RequestParams(
+        request_params.get("token"), request_params.get("group_id"))
     objects["request_params"] = request_params_obj
     return objects
